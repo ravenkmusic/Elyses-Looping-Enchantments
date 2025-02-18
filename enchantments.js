5890ac38f184564f785bd56cd4381c1d2558ea26
Raven Hendricks <ravenkbeats@gmail.com>
2025-02-18 08:12:15 -0500

    completed cardtype function

--- enchantments.js
+++ enchantments.js
@@ -9,16 +9,22 @@
  * @returns {number} number of cards of a single type there are in the deck
  */
 export function cardTypeCheck(stack, card) {
-    throw new Error('Implement the cardTypeCheck function');
-  }
-  
-  /**
-   * Determine how many cards are odd or even
-   *
-   * @param {number[]} stack
-   * @param {boolean} type the type of value to check for - odd or even
-   * @returns {number} number of cards that are either odd or even (depending on `type`)
-   */
-  export function determineOddEvenCards(stack, type) {
-    throw new Error('Implement the determineOddEvenCards function');
-  }  
+  let cardType = 0;
+  stack.forEach((thisCard) => {
+    if (thisCard === card) {
+      cardType ++;
+    }
+  });
+  return cardType;
+}
+
+/**
+ * Determine how many cards are odd or even
+ *
+ * @param {number[]} stack
+ * @param {boolean} type the type of value to check for - odd or even
+ * @returns {number} number of cards that are either odd or even (depending on `type`)
+ */
+export function determineOddEvenCards(stack, type) {
+  throw new Error('Implement the determineOddEvenCards function');
+}
